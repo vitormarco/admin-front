29b481a5532b807d6d454ada3692acce597a827e
vitor-marco <vitormarco@github.com>
2019-04-07 14:48:15 -0300

    building the structure

---
+++ src/modules/admin/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <main id="admin">
+            <h2>Hello World</h2>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'admin'
+}
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,5 @@
+import {routes as admin} from '../modules/admin/router'
+
+export default [
+  ...admin
+]
