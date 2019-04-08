b3658a2a7f9598bd9459f17da4edff887eedacaf
vitor-marco <vitormarco@github.com>
2019-04-07 23:22:35 -0300

    List

--- src/modules/admin/components/dash/Main.vue
+++ src/modules/admin/components/dash/Main.vue
@@ -1,7 +1,20 @@
 <template>
     <div>
         <main id="dash">
-            <h2>Hello</h2>
+            <b-container>
+                <b-row align-h="center">
+                    <div v-for="(item, index) in this.users" :key="index" class="w-100">
+                        <b-button v-b-toggle.collapse-1 variant="primary" class="w-100">USER: {{ item.name }}</b-button>
+                        <b-collapse id="collapse-1" class="mt-2">
+                          <b-card>
+                                <b-form-group :label="`Name: ${item.name}`"></b-form-group>
+                                <b-form-group :label="`Email address: ${item.email}`"></b-form-group>
+                                <b-form-group :label="`Permission: ${itemRoles.name}`" v-for="(itemRoles, indexRoles) in item.roles" :key="`roles-${indexRoles}`"></b-form-group>
+                          </b-card>
+                        </b-collapse>
+                    </div>
+                </b-row>
+            </b-container>
         </main>
     </div>
 </template>
@@ -9,6 +22,29 @@
 <script>
 
 export default {
-  name: 'dash'
+  name: 'dash',
+  data () {
+    return {
+      users: {},
+      email: '',
+      password: '',
+      name: ''
+    }
+  },
+  mounted () {
+    const self = this
+    this.$axios.get('/users', {
+      headers: {
+        Authorization: 'Bearer ' + sessionStorage.getItem('token')
+      }
+    })
+      .then(function (success) {
+        self.users = success.data
+        console.log(self.users)
+      })
+      .catch(function (error) {
+        alert(error)
+      })
+  }
 }
 </script>
