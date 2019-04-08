5e205eb766d46fe0260eb9229d7bb562b7d429d9
vitor-marco <vitormarco@github.com>
2019-04-07 23:53:13 -0300

    fix

--- src/modules/admin/components/dash/Main.vue
+++ src/modules/admin/components/dash/Main.vue
@@ -4,14 +4,9 @@
             <b-container>
                 <b-row align-h="center">
                     <div v-for="(item, index) in this.users" :key="index" class="w-100">
-                        <b-button v-b-toggle.collapse-1 variant="primary" class="w-100">USER: {{ item.name }}</b-button>
-                        <b-collapse id="collapse-1" class="mt-2">
-                          <b-card>
-                                <b-form-group :label="`Name: ${item.name}`"></b-form-group>
-                                <b-form-group :label="`Email address: ${item.email}`"></b-form-group>
-                                <b-form-group :label="`Permission: ${itemRoles.name}`" v-for="(itemRoles, indexRoles) in item.roles" :key="`roles-${indexRoles}`"></b-form-group>
-                          </b-card>
-                        </b-collapse>
+                        <b-form-group :label="`Name: ${item.name}`"></b-form-group>
+                        <b-form-group :label="`Email address: ${item.email}`"></b-form-group>
+                        <b-form-group :label="`Permission: ${itemRoles.name}`" v-for="(itemRoles, indexRoles) in item.roles" :key="`roles-${indexRoles}`"></b-form-group>
                     </div>
                 </b-row>
             </b-container>
